fe0e136a54e2659f91cc12e4bce08f81cda0becb
Levtraru <107771213+Levtraru@users.noreply.github.com>
2023-01-06 12:48:36 -0300

    comment functions

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 window.addEventListener("load", () => {
   // Language change function
+  /** 
+   * Toggles between English and Spanish by clicking the language button
+   * in the navbar
+   */
   $(".lang-en").hide();
   $("#switch-lang").click(function () {
     $(".lang-en").toggle();
@@ -8,6 +12,10 @@ window.addEventListener("load", () => {
 });
 
 // Training Tabs
+/**
+ * Used in the section: About Me — My Training.
+ * Displays the information of each tab.
+ */
 function openTab(tabName){
   var x = $(".training-desc");
   for (let i = 0; i < x.length; i++) {
